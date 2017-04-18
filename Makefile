bc3bdcb73bbef7cf3de98cc853e5cf8af55a6c1e
Romain Dorgueil <romain@dorgueil.net>
2017-04-18 15:49:58 +0200

    auto: generated code

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # This file has been auto-generated.
 # All changes will be lost, see Projectfile.
 #
-# Updated at 2017-04-18 15:49:33.481966
+# Updated at 2017-04-18 15:49:56.787527
 
 ENV ?= production
 PYTHON ?= $(shell which python)
--- config/settings/local.py
+++ config/settings/local.py
@@ -23,7 +23,7 @@ TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 # Note: This key only used for development and testing.
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='VhIYs.hXk_pVJ=7ITyo?>-[Gf!WU&aAWwnGp|~@gig?*xPkMgV')
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='*5*XnsPyc4;Z/_:!eupMhI}s<SCOR-][MrsZeg&nM=>1x@1p=L')
 
 # Mail settings
 # ------------------------------------------------------------------------------
